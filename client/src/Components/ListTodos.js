ab54f1fe3a3712b3dfb738a2a33b0409bf59a8dd
lvictor taylor <lvictor.taylor@gmail.com>
2023-10-08 21:39:19 -0400

    Update ListTodos.js

--- client/src/Components/ListTodos.js
+++ client/src/Components/ListTodos.js
@@ -24,7 +24,7 @@ const ListTodos = () => {
     const getTodos = async () => {
         try {
             
-            const response = await fetch(`https://newserver5.vercel.app/todos` , {
+            const response = await fetch(`https://newserver66-phjv6c9db-lvictors-projects.vercel.app/todos` , {
                 "mode" :"no-cors"
             })
             const jsonData = await response.json()
