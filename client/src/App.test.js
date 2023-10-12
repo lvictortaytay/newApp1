cc123e081df03ec4087186ea106817345c968ad9
lvictor taylor <lvictor.taylor@gmail.com>
2023-10-11 23:08:39 -0400

    Update App.test.js

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,6 +1,7 @@
 import React from "react"
 import {render} from "@testing-library/react"
 import App from "./App"
+import { MemoryRouter  } from 'react-router-dom/cjs/react-router-dom.min';
 
 
 
@@ -8,3 +9,34 @@ it("should render without exploding" , ()=>{
     render(<App/>)
 })
 
+
+test('/todos route ', () => {
+    const {getByText}  = render((
+         <MemoryRouter initialEntries = {["/todos"]}>
+                 <App />
+         </MemoryRouter>
+   
+         
+   
+   
+   
+     ));
+     expect(getByText("edit")).toBeInTheDocument()
+   });
+   
+   
+   test("if edit button works" , () => {
+     const {getByText} = render((
+       <MemoryRouter initialEntries = {["/todos"]}>
+         <App/>
+       </MemoryRouter>
+     ))
+   
+     expect(getByText("random")).toBeInTheDocument()
+   
+     const aboutLink = getByText("edit")
+     fireEvent.click(aboutLink)
+   
+     expect(getByText("save changes")).toBeInTheDocument()
+   })
+   
